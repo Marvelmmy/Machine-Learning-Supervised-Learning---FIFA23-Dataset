e804ad2fa28c8667509fc7cefe1fd409ceb23ea6
Marvelmmy <marvelmamuaya@gmail.com>
2025-04-24 11:31:50 +0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,9 @@
+#importing libraries 
+import numpy as np 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, f1_score
+from sklearn.preprocessing import StandardScaler
+
+#define the targeted output (y >= 75)
