84951c0e554e6f878859cf4df80edf3dc300c918
Marvelmmy <marvelmamuaya@gmail.com>
2025-04-28 08:21:16 +0700

    Update model.py

--- model.py
+++ model.py
@@ -7,3 +7,9 @@ from sklearn.metrics import classification_report, f1_score
 from sklearn.preprocessing import StandardScaler
 
 #define the targeted output (y >= 75)
+df['Good'] = df['Overall'].apply(lambda x: if x >= 75 else 0)
+#identifying the features
+ml_features = df.drop(columns=['Good','Overall'])
+ml_labels = df['Good']
+X = ml_features.select_dtypes(include=['number'])
+y = ml_labels
